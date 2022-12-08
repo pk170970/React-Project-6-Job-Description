4cbec8760795006171e5a7a3980a52efd7181478
Code Sandbox <hello@codesandbox.io>
2022-12-08 11:18:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4477dac224c684d08c5c32a590fe97eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8959c4e262ac57c69cc7e218fefdcebc",
+    "url": "https://pk170970.github.io/csb-zhl5vr/index.html"
+  },
+  {
+    "revision": "e79dea189a1aa8ded99e",
+    "url": "https://pk170970.github.io/csb-zhl5vr/static/css/main.fd50c553.chunk.css"
+  },
+  {
+    "revision": "07b795ec504bed5c24ad",
+    "url": "https://pk170970.github.io/csb-zhl5vr/static/js/2.8c7fcccd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://pk170970.github.io/csb-zhl5vr/static/js/2.8c7fcccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79dea189a1aa8ded99e",
+    "url": "https://pk170970.github.io/csb-zhl5vr/static/js/main.816b565d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://pk170970.github.io/csb-zhl5vr/static/js/main.816b565d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367edd24af78d90bac7b",
+    "url": "https://pk170970.github.io/csb-zhl5vr/static/js/runtime-main.2f78360c.js"
+  }
+]);
